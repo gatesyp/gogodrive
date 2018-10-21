88ea7c8d626da777bf563060c5887a7e99cadc7f
agates4 <agate97@gmail.com>
2018-10-21 05:36:03 -0400

    settings backgrondcolor

--- Settings.js
+++ Settings.js
@@ -19,7 +19,7 @@ export default class Settings extends React.Component {
 
     render() {
         return (
-            <Container style={{width: "100%", height: "100%"}}>
+            <Container style={{width: "100%", height: "100%", backgroundColor: "#F5F5F5"}}>
                 <Toolbar
                     isHidden={false}
                 />
